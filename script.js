a65be1640f4597f30a65035d5f7694c5e9c531e8
bpn <bipinlamsal2004@gmail.com>
2024-02-17 01:36:32 +0530

    little fix

---
+++ script.js
@@ -0,0 +1,14 @@
+url = 'https://api.github.com/users/bipin333/repos'
+container = document.getElementById('project')
+fetch(url).then(r => r.json()).then(datas =>{
+    datas.forEach(repo =>{
+        lin = document.createElement('a');
+        lin.href = repo.svn_url
+        lin.className = 'proje'
+        tit = document.createElement('h3')
+        tit.textContent = repo.full_name
+
+        lin.appendChild(tit)
+        container.appendChild(lin)
+    })
+})
