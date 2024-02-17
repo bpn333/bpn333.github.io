ff72b432e50f25404d62bd3211b96472edf91258
bpn <bipinlamsal2004@gmail.com>
2024-02-18 00:36:34 +0530

    op

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 url = 'https://api.github.com/users/bipin333/repos'
-container = document.getElementById('project')
+container = document.getElementById('projects')
 fetch(url).then(r => r.json()).then(datas =>{
     datas.forEach(repo =>{
         lin = document.createElement('a');
         lin.href = repo.svn_url
         lin.className = 'proje'
         tit = document.createElement('h3')
-        tit.className = 'projetitle'
         tit.textContent = repo.full_name
         lin.appendChild(tit)
         container.appendChild(lin)
