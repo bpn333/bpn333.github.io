6e5aa5cd05a926cb1b433d4cf8dca2ca71ded847
bpn <bipinlamsal2004@gmail.com>
2024-02-17 20:09:00 +0530

    domain work 🙏

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ fetch(url).then(r => r.json()).then(datas =>{
         lin.href = repo.svn_url
         lin.className = 'proje'
         tit = document.createElement('h3')
+        tit.className = 'projetitle'
         tit.textContent = repo.full_name
-
         lin.appendChild(tit)
         container.appendChild(lin)
     })
