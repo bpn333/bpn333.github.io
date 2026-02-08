d11e0b66a84325b8171f5020c87b713ca441f87a
bpn <bipinlamsal2004@gmail.com>
2026-02-08 15:24:34 +0530

    .

--- src/data/experience.js
+++ src/data/experience.js
@@ -13,7 +13,7 @@ const experience = [
       "Linux",
     ],
     testimonial: {
-      text: "An intern, far more skilled than expected, Bipin is smart, driven, and refreshingly hardworking. What usually takes seniors two to three years to crack, he tackled confidently in just a few months. Quick to learn and quicker to deliver, he turns challenges into wins with ease. With sharp focus, strong ownership, and a can-do attitude, he's already proving that great talent doesn't wait - “it arrives ready.",
+      text: "An intern, far more skilled than expected, Bipin is smart, driven, and refreshingly hardworking. What usually takes seniors two to three years to crack, he tackled confidently in just a few months. Quick to learn and quicker to deliver, he turns challenges into wins with ease. With sharp focus, strong ownership, and a can-do attitude, he's already proving that great talent, doesn't wait it arrives ready.",
       source: "https://webmediainfotech.com/about-us",
     },
   },
